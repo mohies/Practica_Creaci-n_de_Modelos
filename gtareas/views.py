59a41e1fd9a2090867799c4cb8eb0b358fcd895b
mohies <mohcen.benizza.bouzid.al@iespoligonosur.org>
2024-10-23 20:05:10 +0200

    index hecho

--- gtareas/views.py
+++ gtareas/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import *
+from django.db.models import Q
+from django.db.models import Avg,Max,Min
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
